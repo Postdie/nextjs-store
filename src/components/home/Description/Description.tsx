871faec82bef9a2940978fd7de50369bf5dc1ed6
Postdie <fernandezdiego845@gmail.com>
2024-02-26 04:08:47 -0600

    decimoSengundo commit(2_5Menejo y y optimizacion de imagenes)

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,9 +1,19 @@
+import styles from './Description.module.sass';
+import Image from 'next/image';
+
 export const Description = () => {
     return(
-        <section>
-            <img src="/images/FutureWorld.jpg" alt="products marketplace" />
-            <h2>Descripción</h2>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur, quidem debitis a doloribus quam voluptates praesentium dolorum accusamus veritatis obcaecati, totam possimus architecto cum minus nemo? Dolorem molestias necessitatibus praesentium.</p>
+        <section className='{styles.Description}'>
+            <Image 
+                src="/images/FutureWorld.jpg" 
+                alt="products marketplace" 
+                width={500} 
+                height={300}
+            />
+            <div className={styles.Description__text}>
+                <h2>Descripción</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur, quidem debitis a doloribus quam voluptates praesentium dolorum accusamus veritatis obcaecati, totam possimus architecto cum minus nemo? Dolorem molestias necessitatibus praesentium.</p>
+            </div>
         </section>
     )
 }
