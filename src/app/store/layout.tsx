0c65c626d0f451ae188018f25a07c59645a57d5f
Postdie <fernandezdiego845@gmail.com>
2024-02-25 22:11:21 -0600

    Tercer commit(Video5)

---
+++ src/app/store/layout.tsx
@@ -0,0 +1,9 @@
+export default function Layout({ children }: {children: React.ReactNode}) {
+
+    return (
+        <main>
+            <nav>Navegacion de la categorias</nav>
+            {children}
+        </main>
+    )
+}
