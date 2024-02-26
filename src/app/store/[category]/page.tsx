bc0832b2a4bba27691a235d16ccdbc7b1deeaf45
Postdie <fernandezdiego845@gmail.com>
2024-02-25 21:59:45 -0600

    Segundo commit(Video3)

---
+++ src/app/store/[category]/page.tsx
@@ -0,0 +1,16 @@
+interface CategoryProps {
+    params: {
+        category: string
+    }
+}
+
+export default function Category(props: CategoryProps){
+
+    const { category } = props.params
+
+    return (
+
+        <h1> Categoria dinamica: {category} </h1>
+
+    )
+}
---
+++ src/app/store/page.tsx
@@ -0,0 +1,8 @@
+export default function store(){
+
+    return (
+
+        <h1> Store </h1>
+
+    )
+}
