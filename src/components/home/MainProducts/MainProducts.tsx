ba70f1d8998af8f3e48bc34817a157cefa6bec23
Postdie <fernandezdiego845@gmail.com>
2024-03-11 01:38:00 -0600

    decimoSeptimo commit(3_2ComoObtenerInformacionDeUnaAPI)

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,9 +1,17 @@
-"use public"
+const getProducts = async () => {
 
-export const MainProducts = () => {
+    const response = await fetch(`${process.env.SHOPIFY_HOSTNAME}/admin/api/2024-01/graphql.json`, {
+        headers:  new Headers({
+            'X-Shopify-Access-Token': process.env.SHOPIFY_API_KEY || "",
+        })
+    });
+    const data = await response.json()
+    return data
+}
 
-    console.log(process.env.NEXT_PUBLIC_SHOPIFY_HOSTNAME)
-
+export const MainProducts = async () => {
+    const products = await getProducts()
+    console.log(products)
     return(
         <section>
             <h1>Main Products</h1>
