f211bc9a9abcb870fd030e3cf580edffed5ff59f
Postdie <fernandezdiego845@gmail.com>
2024-03-20 02:04:24 -0600

    decimoSeptimo-2 commit(3_3ComoObtenerInformacionDeUnaAPI)

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,8 +1,8 @@
 const getProducts = async () => {
 
-    const response = await fetch(`${process.env.SHOPIFY_HOSTNAME}/admin/api/2024-01/graphql.json`, {
+    const response = await fetch(`${process.env.SHOPIFY_HOSTNAME}/admin/api/2024-01/products/11235813213455.json`, {
         headers:  new Headers({
-            'X-Shopify-Access-Token': process.env.SHOPIFY_API_KEY || "",
+            'X-Shopify-Access-Token': process.env.SHOPIFY_API_KEY || ""
         })
     });
     const data = await response.json()
