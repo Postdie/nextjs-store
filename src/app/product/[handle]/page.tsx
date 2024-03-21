659441f733ec47f3007f72f16068b2d04cdb44cd
Postdie <fernandezdiego845@gmail.com>
2024-03-21 02:31:13 -0600

    Trigesimo commit(4_10PaginadeProductoYArreglosDeSitio)

--- src/app/product/[handle]/page.tsx
+++ src/app/product/[handle]/page.tsx
@@ -1,11 +1,16 @@
-"use client"
+import { ProductView } from "app/components/product/ProductView"
+import { getProducts } from "app/services/shopify/products"
 
-import { useParams, useSearchParams } from "next/navigation"
+interface ProductPageProps {
+    searchParams: {
+        id?: string
+    }
+}
+export default async function ProductPage({searchParams}: ProductPageProps){
 
-export default function ProductPage(){
-    const params = useParams()
-    const searchParams = useSearchParams()
-    const id = searchParams.get("id")
-    console.log('searchParams', id)
-    return <h1>Product Page</h1>
+        const id = searchParams.id
+        const products = await getProducts(id)
+        const product = products[0]
+
+    return <ProductView product={product}/>
 }
