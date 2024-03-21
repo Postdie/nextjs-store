1f19537a3c64e0f492d6277111651246b1ab7991
Postdie <fernandezdiego845@gmail.com>
2024-03-21 01:26:38 -0600

    VigesimoOctavo commit(4_8FiltrandoCategoriasDeProductos)

--- src/app/store/[[...categories]]/page.tsx
+++ src/app/store/[[...categories]]/page.tsx
@@ -1,26 +1,27 @@
 import { ProductsWrapper } from "app/components/Store/ProductsWrapper"
+import { getCollectionProducts, getCollections } from "app/services/shopify/collections"
 import { getProducts } from "app/services/shopify/products"
 
 interface CategoryProps {
-    
-    params: {
-
-        categories: string[],
-
-        searchParams?: string
-
-    }
+  params: {
+    categories: string[],
+  }
+  searchParams?: string
 }
 
-export default async function category(props: CategoryProps){
-    const products = await getProducts()
-    const { categories } = props.params
+export default async function Category(props: CategoryProps) {
+  const { categories } = props.params
+  let products = []
+  const collections = await getCollections()
+  
+  if (categories?.length > 0) {
+    const selectedCollectionId = collections.find((collection: any) => collection.handle === categories[0]).id
+    products = await getCollectionProducts(selectedCollectionId)
+  }else {
+    products = await getProducts()
+  }
 
-    //throw new Error('Error: Boom!')
-
-    return (
-
-        <ProductsWrapper products={products}/>
-
-    )
+  return (
+    <ProductsWrapper products={products} />
+  )
 }
