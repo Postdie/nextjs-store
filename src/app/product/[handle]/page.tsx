02e2761c30625fc7c4afcdf81e898b8b4c204ed4
Postdie <fernandezdiego845@gmail.com>
2024-03-21 04:29:40 -0600

    TrigesimoQuinto commit(4_15MejorandoSEODeUnaPagina)

--- src/app/product/[handle]/page.tsx
+++ src/app/product/[handle]/page.tsx
@@ -7,6 +7,22 @@ interface ProductPageProps {
         id?: string
     }
 }
+
+export async function generateMetadata({ searchParams }: ProductPageProps) { 
+    const id = searchParams.id
+    const products = await getProducts(id)
+    const product = products[0]
+  
+    return {
+      title: product.title,
+      description: product.description,
+      keywords: product.tags,
+      openGraph: {
+        images: [product.image]
+      }
+    }
+  }
+
 export default async function ProductPage({searchParams}: ProductPageProps){
 
         const id = searchParams.id
@@ -14,7 +30,7 @@ export default async function ProductPage({searchParams}: ProductPageProps){
         const product = products[0]
 
         if(!id){
-            redirect('/store')
+            redirect('/')
         }
 
     return <ProductView product={product}/>
